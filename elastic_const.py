3fa84163cd64657aac29e5662f4dcebbe0122593
Azat Nizametdinov <anizametdinov@gmail.com>
2015-03-11 23:55:29 +0300

    Implement execution of FEM simulation, add TripletForces class

--- elastic_const.py
+++ elastic_const.py
@@ -1,6 +1,8 @@
 from scipy.misc import derivative
 from subprocess import Popen, PIPE
+from os import path
 import re
+import numpy as np
 
 STEP = 0.1
 ORDER = 5
@@ -9,26 +11,46 @@ PROC_ENCODING = 'cp866'
 RESULT_PATTERN = ('^\s*' + '(-?\d+\.\d+)\s+' * 9 +
     ''.join('(?P<f{0}x>-?\d+\.\d+)\s+(?P<f{0}y>-?\d+\.\d+)\s+(-?\d+\.\d+)\s+'.format(i) for i in range(1, 4)) +
     '(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s*')
+CONFIG_FILE = 'triplet_config.txt'
 
-class Cache(object):
-    def __init__(self):
-        pass
+def dist_sqr(p1, p2):
+    return np.sum((p1 - p2) ** 2)
 
-    def save_result(self, result):
-        pass
+class TripletForces(object):
+    def __init__(self, positions, forces):
+        self.positions = positions
+        self.f1x, self.f1y, self.f2x, self.f2y, self.f3x, self.f3y = forces
+
+    def force(particle_num, axis):
+        """
+        Returns force acting on particle with given number along given axis.
+        Parameters:
+        particle_num: 1, 2, 3
+        axis: 'x' or 'y'
+        """
+        getattr(self, 'f{0}{1}'.format(particle_num, axis))
+
+class ForceCache(object):
+    "This class stores computed forces"
+
+    def __init__(self, working_dir):
+        self.values = []
+
+    def save_result(self, triplet_forces):
+        self.values.append(triplet_forces)
 
     def read(self, positions):
-        pass
+        next((f for f in self.values if f.positions == positions), None)
 
 class FemSimulation(object):
     def __init__(self, command_line, working_dir):
         self.cwd = working_dir
         self.command = command_line
-        self.config_file = 'config.txt'
+        self.config_file = path.join(working_dir, CONFIG_FILE)
         self.pattern = re.compile(RESULT_PATTERN)
 
     def __process_result(self, match):
-        return dict([group, float(match.group(group))] for group in ['f1x', 'f1y', 'f2x', 'f2y', 'f3x', 'f3y'])
+        return [float(match.group(group)) for group in ['f1x', 'f1y', 'f2x', 'f2y', 'f3x', 'f3y']]
 
     def __execute(self):
         proc = Popen(self.command, stdout = PIPE, cwd = self.cwd)
@@ -41,11 +63,16 @@ class FemSimulation(object):
 
     def compute_forces(self, x1, y1, x2, y2, x3, y3):
         "Computes forces acting on particles within given configuration"
-        # Поиск конфигурации в кэше
-        return self.__execute()
-        # Вывод результата в виде {'f1x': 1.1, 'f1y': 1.2, 'f2x': ...}
+        with open(self.config_file, 'w', encoding = FILE_ENCODING) as conf_file:
+            conf_file.write('x1 = {0}\n'.format(x1))
+            conf_file.write('y1 = {0}\n'.format(y1))
+            conf_file.write('x2 = {0}\n'.format(x2))
+            conf_file.write('y2 = {0}\n'.format(y2))
+            conf_file.write('x3 = {0}\n'.format(x3))
+            conf_file.write('y3 = {0}\n'.format(y3))
+        return TripletForces([x1, y1, x2, y2, x3, y3], self.__execute())
 
-def derivative_of_force(particle_num, axis, positions):
+def derivative_of_force(simulation, particle_num, axis, positions, order = ORDER):
     """
     Parameters:
     particle_num: 1, 2, 3
@@ -56,6 +83,6 @@ def derivative_of_force(particle_num, axis, positions):
     positions = list(positions)
     def force_func(arg):
         positions[variable_num] = arg
-        return compute_forces(*positions)['f' + axis + str(particle_num)]
-    return derivative(force_func, positions[variable_num], dx=STEP, order=ORDER)
+        return simulation.compute_forces(*positions).force(particle_num, axis)
+    return derivative(force_func, positions[variable_num], dx=STEP, order=order)
 
--- test.py
+++ test.py
@@ -1,21 +1,40 @@
 import elastic_const as ec
 import unittest
-from os import path
+import tempfile
+import os
 
 class TestFemSimulation(unittest.TestCase):
     def setUp(self):
-        dirname = path.dirname(path.abspath(__file__))
-        example_file = path.join(dirname, 'test_data\\output_example.txt')
+        dirname = os.path.dirname(os.path.abspath(__file__))
+        example_file = os.path.join(dirname, 'test_data\\output_example.txt')
         command = ['cmd', '/C', 'type', example_file]
         self.subject = ec.FemSimulation(command, dirname)
+        f, self.config_file = tempfile.mkstemp()
+        self.subject.config_file = self.config_file
+        os.close(f)
 
     def test_compute_forces(self):
-        forces = self.subject.compute_forces(0, 0, 1, 1, 2, 2)
-        self.assertEqual(forces, {
-            'f1x': -0.17074827, 'f1y': -0.01507202,
-            'f2x': -0.17909645, 'f2y': -1.93233496,
-            'f3x': 0.34981181, 'f3y': 1.94751238
-            })
+        forces = self.subject.compute_forces(0, 1, 2, 3, 4, 5)
+        self.assertEqual(forces.f1x, -0.17074827)
+        self.assertEqual(forces.f1y, -0.01507202)
+        self.assertEqual(forces.f2x, -0.17909645)
+        self.assertEqual(forces.f2y, -1.93233496)
+        self.assertEqual(forces.f3x, 0.34981181)
+        self.assertEqual(forces.f3y, 1.94751238)
+
+        conf = None
+        with open(self.config_file, encoding = ec.FILE_ENCODING) as conf_file:
+            conf = list(map(str.strip, conf_file.readlines()))
+        self.assertEqual(len(conf), 6)
+        self.assertIn('x1 = 0', conf)
+        self.assertIn('y1 = 1', conf)
+        self.assertIn('x2 = 2', conf)
+        self.assertIn('y2 = 3', conf)
+        self.assertIn('x3 = 4', conf)
+        self.assertIn('y3 = 5', conf)
+
+    def tearDown(self):
+        os.remove(self.config_file)
 
 if __name__ == "__main__":
     unittest.main()
