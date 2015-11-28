54ad8cf1977ec5a7591035fd3a7d528b88208b98
Azat Nizametdinov <anizametdinov@gmail.com>
2015-11-28 16:19:41 +0300

    Implement rotation of triplet force derivatives for second particle

--- elastic_const/triplet_force_derivatives.py
+++ elastic_const/triplet_force_derivatives.py
@@ -1,8 +1,7 @@
 from scipy.misc import derivative
 from os import path
 from elastic_const.cache_base import CacheBase
-from elastic_const.misc import format_float, euclidean_distance, pairs
-from collections import namedtuple
+from elastic_const.misc import format_float, euclidean_distance, pairwise_distances, cross_product_2d
 import numpy as np
 import logging
 
@@ -18,14 +17,16 @@ class TripletForceDerivatives(object):
         self.axis = axis.lower()
         self.derivatives = derivatives
 
-    def derivative(self, particle_num, axis):
+    def derivative(self, particle_num, axis=None):
         """
         Returns derivative of force acting on particle with given number along given axis.
         Parameters:
         particle_num: 1, 2, 3
-        axis: 'x' or 'y'
+        axis: 'x', 'y' or None
         """
         variable_num = (particle_num - 1) * 2
+        if axis is None:
+            return np.array(self.derivatives[variable_num:variable_num+2])
         if axis.lower() == 'y':
             variable_num += 1
         return self.derivatives[variable_num]
@@ -59,21 +60,43 @@ class TripletForceDerivatives(object):
 
 
 class TripletForceDerivativeCache(CacheBase):
-    "This class stores computed force derivatives"
+    """This class stores computed force derivatives"""
 
-    def __init__(self, working_dir, cache_file=None):
+    def __init__(self, working_dir, triplet_fdc, cache_file=None):
         cache_file_path = cache_file or path.join(working_dir, DERIVATIVE_CACHE_FILE)
         super().__init__(cache_file_path)
+        self.sets = []
+        self.triplet_fdc = triplet_fdc
 
     def _value_from_string(self, string):
         return TripletForceDerivatives.from_string(string)
 
+    def __find_set(self, positions):
+        result = next((s for s in self.sets if np.allclose(s.positions, positions)), None)
+        if result is None:
+            result = TripletDerivativeSet(positions, self.triplet_fdc)
+            self.sets.append(result)
+        return result
+
+    def __deduce_from_set(self, axis, particle_num, positions):
+        sorted_distances = sorted(pairwise_distances(positions))
+        derivative_set = next((s for s in self.sets if np.allclose(s.sorted_distances, sorted_distances)), None)
+        if derivative_set:
+            return derivative_set.try_deduce(axis, particle_num, positions)
+        else:
+            return None
+
+    def save_result(self, value):
+        super().save_result(value)
+        self.__find_set(value.positions).add_derivatives(value)
+
     def read(self, axis, particle_num, positions):
         axis = axis.lower()
-        return next(
+        result = next(
             (fd for fd in self.values if fd.have_coords(axis, particle_num, positions)),
             None
         )
+        return result or self.__deduce_from_set(axis, particle_num, positions)
 
 
 class TripletForceDerivativeComputation(object):
@@ -82,7 +105,7 @@ class TripletForceDerivativeComputation(object):
         self.simulation = simulation
         self.order = order
         self.step = step
-        self.cache = TripletForceDerivativeCache(working_dir)
+        self.cache = TripletForceDerivativeCache(working_dir, self)
         self.r = r
         self.derivative_func = derivative_func
 
@@ -124,3 +147,87 @@ class TripletForceDerivativeComputation(object):
         if min_dist - 2 * self.r < 1.0:
             return self.step * (min_dist - 2 * self.r)
         return self.step
+
+
+class TripletDerivativeSet(object):
+    """This class stores set of derivatives for triplets with the save positions"""
+
+    def __init__(self, positions, triplet_fdc):
+        self.positions = np.copy(positions)
+        self.sorted_distances = sorted(pairwise_distances(positions))
+        self.derivatives = {}
+        self.triplet_fdc = triplet_fdc
+        if self.triplet_fdc:
+            self.forces = self.triplet_fdc.simulation.compute_forces(positions)
+
+    def add_derivatives(self, force_derivatives):
+        assert np.allclose(self.positions, force_derivatives.positions)
+        variable = force_derivatives.axis + str(force_derivatives.particle_num)
+        self.derivatives[variable] = force_derivatives
+
+    def try_deduce(self, axis, particle_num, positions):
+        # Renumbering
+        # positions[0] should be [0, 0]
+        r12 = np.linalg.norm(positions[1])
+        cos_theta = positions[1].dot(self.positions[1]) / (r12 * r12)
+        sin_theta = cross_product_2d(self.positions[1], positions[1]) / (r12 * r12)
+        transform_matrix = np.array([
+            [cos_theta, -sin_theta],
+            [sin_theta,  cos_theta]
+        ])
+        inverse_transform = transform_matrix.T
+        if particle_num == 2:
+            coord_num = {'x': 0, 'y': 1, 'z': 2}[axis]
+            pair_coord_num = {0: 1, 1: 0}[coord_num]
+            x2, y2, *_ = positions[1]
+            x3, y3, *_ = positions[2]
+
+            dcos_theta = self.positions[1][coord_num] * r12 ** 2
+            dcos_theta -= positions[1][coord_num] * positions[1].dot(self.positions[1])
+            dcos_theta /= r12 ** 4
+            dsin_theta = (-1) ** (coord_num + 1) * self.positions[1][pair_coord_num] * r12 ** 2
+            dsin_theta -= positions[1][coord_num] * cross_product_2d(self.positions[1], positions[1])
+            dsin_theta /= (r12 ** 4)
+
+            dp02 = np.array([
+                x2 * dcos_theta + y2 * dsin_theta,
+                -x2 * dsin_theta + y2 * dcos_theta
+            ]) + inverse_transform[:, coord_num]
+            dp03 = np.array([
+                x3 * dcos_theta + y3 * dsin_theta,
+                -x3 * dsin_theta + y3 * dcos_theta
+            ])
+
+            F01x = self.forces.force(1, 'x')
+            F01y = self.forces.force(1, 'y')
+            F02x = self.forces.force(2, 'x')
+            F02y = self.forces.force(2, 'y')
+            F03x = self.forces.force(3, 'x')
+            F03y = self.forces.force(3, 'y')
+
+            deriv_matrix_2_2 = np.array([self.derivatives['x2'].derivative(2), self.derivatives['y2'].derivative(2)]).T
+            deriv_matrix_3_2 = np.array([self.derivatives['x3'].derivative(2), self.derivatives['y3'].derivative(2)]).T
+            dF02x_dx2, dF02y_dx2 = deriv_matrix_2_2.dot(dp02) + deriv_matrix_3_2.dot(dp03)
+
+            dF2x_dx2 = dF02x_dx2 * cos_theta - dF02y_dx2 * sin_theta + F02x * dcos_theta - F02y * dsin_theta
+            dF2y_dx2 = dF02x_dx2 * sin_theta + dF02y_dx2 * cos_theta + F02x * dsin_theta + F02y * dcos_theta
+
+            deriv_matrix_2_3 = np.array([self.derivatives['x2'].derivative(3), self.derivatives['y2'].derivative(3)]).T
+            deriv_matrix_3_3 = np.array([self.derivatives['x3'].derivative(3), self.derivatives['y3'].derivative(3)]).T
+            dF03x_dx2, dF03y_dx2 = deriv_matrix_2_3.dot(dp02) + deriv_matrix_3_3.dot(dp03)
+
+            dF3x_dx2 = dF03x_dx2 * cos_theta - dF03y_dx2 * sin_theta + F03x * dcos_theta - F03y * dsin_theta
+            dF3y_dx2 = dF03x_dx2 * sin_theta + dF03y_dx2 * cos_theta + F03x * dsin_theta + F03y * dcos_theta
+
+            deriv_matrix_2_1 = np.array([self.derivatives['x2'].derivative(1), self.derivatives['y2'].derivative(1)]).T
+            deriv_matrix_3_1 = np.array([self.derivatives['x3'].derivative(1), self.derivatives['y3'].derivative(1)]).T
+            dF01x_dx2, dF01y_dx2 = deriv_matrix_2_1.dot(dp02) + deriv_matrix_3_1.dot(dp03)
+
+            dF1x_dx2 = dF01x_dx2 * cos_theta - dF01y_dx2 * sin_theta + F01x * dcos_theta - F01y * dsin_theta
+            dF1y_dx2 = dF01x_dx2 * sin_theta + dF01y_dx2 * cos_theta + F01x * dsin_theta + F01y * dcos_theta
+
+            return TripletForceDerivatives(
+                axis, particle_num, positions, np.array([dF1x_dx2, dF1y_dx2, dF2x_dx2, dF2y_dx2, dF3x_dx2, dF3y_dx2])
+            )
+
+        return None
--- tests/test_triplet_force_derivatives.py
+++ tests/test_triplet_force_derivatives.py
@@ -18,7 +18,7 @@ class TestTripletForceDerivativeCache(unittest.TestCase):
         """
         with open(f, 'w') as cache_file:
             cache_file.write(test_data)
-        self.subject = fd.TripletForceDerivativeCache(None, cache_file=self.cache_file)
+        self.subject = fd.TripletForceDerivativeCache(None, None, cache_file=self.cache_file)
 
     def test_restore_cache(self):
         self.assertEqual(len(self.subject.values), 3)
@@ -106,14 +106,53 @@ class TestTripletForceDerivativeComputation(unittest.TestCase):
 
     def test_uses_smaller_step_for_small_dist(self):
         self.subject.derivative_func = Mock(return_value=np.array([0, 0, 0, 0, 0, 0]))
+        self.subject.simulation.compute_forces = Mock(return_value=None)
 
         self.subject.derivative_of_forces('x', 2, np.array([[0, 0], [2.5, 0], [3, 3]]))
         self.subject.derivative_func.assert_called_with(ANY, 2.5, dx=(2.5 - 2) * 0.01, order=3)
 
+    def test_computes_rotated_configuration_using_cash(self):
+        p2 = np.array([3., 0.])
+        p3 = np.array([2.4, 3.])
+        rotation_matrix = np.array([
+            [np.sqrt(3) / 2, -0.5],
+            [0.5, np.sqrt(3) / 2]
+        ])
+        positions = np.array([[0., 0.], p2, p3])
+        forces = fs.TripletForces(
+            positions, np.array([-3.20695605, -0.63937358, 3.08803874, -2.48336419, 0.11891729, 3.12273782])
+        )
+        self.subject.simulation.compute_forces = Mock(return_value=forces)
+        self.subject.cache.save_result(fd.TripletForceDerivatives(
+            'x', 2, positions,
+            np.array([3.86246709, 1.32785211e-03, -3.14359736, 0.660160262, -0.718868188, -0.661486892])
+        ))
+        self.subject.cache.save_result(fd.TripletForceDerivatives(
+            'y', 2, positions,
+            np.array([0.05438376, -0.89292005, 0.66015376, -2.57570787, -0.7145375, 3.46862663])
+        ))
+        self.subject.cache.save_result(fd.TripletForceDerivatives(
+            'x', 3, positions,
+            np.array([0.29009147, 0.59165673, -0.71886792, -0.71453534, 0.42877687, 0.12287993])
+        ))
+        self.subject.cache.save_result(fd.TripletForceDerivatives(
+            'y', 3, positions,
+            np.array([0.53861846, 0.49871114, -0.66149429, 3.46864056, 0.12287998, -3.96734753])
+        ))
+
+        new_positions = np.array([[0., 0.], rotation_matrix.dot(p2), rotation_matrix.dot(p3)])
+        computed = self.subject.derivative_of_forces('x', 2, new_positions)
+        np_test.assert_allclose(
+            computed.derivatives, [2.64949995, 2.04654145, -3.57334059, 0.08418033, 0.92384189, -2.13072143],
+            atol=0.1
+        )
+
 
 class TestTripletForceDerivatives(unittest.TestCase):
     def setUp(self):
-        self.subject = fd.TripletForceDerivatives('x', 2, np.array([[0, 1], [2, 3], [4, 5]]), np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6]))
+        self.subject = fd.TripletForceDerivatives(
+            'x', 2, np.array([[0, 1], [2, 3], [4, 5]]), np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
+        )
 
     def test_derivative(self):
         self.assertEqual(self.subject.derivative(1, 'x'), 0.1)
@@ -122,6 +161,55 @@ class TestTripletForceDerivatives(unittest.TestCase):
         self.assertEqual(self.subject.derivative(2, 'y'), 0.4)
         self.assertEqual(self.subject.derivative(3, 'x'), 0.5)
         self.assertEqual(self.subject.derivative(3, 'y'), 0.6)
+        np_test.assert_equal(self.subject.derivative(1), np.array([0.1, 0.2]))
+        np_test.assert_equal(self.subject.derivative(2), np.array([0.3, 0.4]))
+        np_test.assert_equal(self.subject.derivative(3), np.array([0.5, 0.6]))
+
+
+class TestTripletDerivativeSet(unittest.TestCase):
+    def setUp(self):
+        self.p2 = np.array([3., 0.])
+        self.p3 = np.array([2.4, 3.])
+        positions = np.array([[0., 0.], self.p2, self.p3])
+        forces = fs.TripletForces(
+            positions, np.array([-3.20695605, -0.63937358, 3.08803874, -2.48336419, 0.11891729, 3.12273782])
+        )
+
+        triplet_fdc = Mock()
+        triplet_fdc.simulation = Mock()
+        triplet_fdc.simulation.compute_forces = Mock(return_value=forces)
+        self.subject = fd.TripletDerivativeSet(positions, triplet_fdc)
+
+        self.subject.add_derivatives(fd.TripletForceDerivatives(
+            'x', 2, positions,
+            np.array([3.86246709, 1.32785211e-03, -3.14359736, 0.660160262, -0.718868188, -0.661486892])
+        ))
+        self.subject.add_derivatives(fd.TripletForceDerivatives(
+            'y', 2, positions,
+            np.array([0.05438376, -0.89292005, 0.66015376, -2.57570787, -0.7145375, 3.46862663])
+        ))
+        self.subject.add_derivatives(fd.TripletForceDerivatives(
+            'x', 3, positions,
+            np.array([0.29009147, 0.59165673, -0.71886792, -0.71453534, 0.42877687, 0.12287993])
+        ))
+        self.subject.add_derivatives(fd.TripletForceDerivatives(
+            'y', 3, positions,
+            np.array([0.53861846, 0.49871114, -0.66149429, 3.46864056, 0.12287998, -3.96734753])
+        ))
+
+    def test_rotated_y2_derivative(self):
+        rotation_matrix = np.array([
+            [np.sqrt(3) / 2, -0.5],
+            [0.5, np.sqrt(3) / 2]
+        ])
+
+        new_positions = np.array([[0., 0.], rotation_matrix.dot(self.p2), rotation_matrix.dot(self.p3)])
+        computed = self.subject.try_deduce('y', 2, new_positions)
+        np_test.assert_allclose(
+            computed.derivatives, [2.09958592, 0.32004116, 0.08419019, -2.14597441, -2.18377565, 1.82592549],
+            atol=0.1
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
